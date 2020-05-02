e1b0d960e96bfe2bb2cf091579f60dbeee6998cc
crazyquark <cristian.sandu@gmail.com>
2020-05-02 20:40:16 +0300

    change freeswitch path

--- freeswitch/start-freeswitch.sh
+++ freeswitch/start-freeswitch.sh
@@ -3,4 +3,4 @@
 until $(nc -z db 5432); do { printf '.'; sleep 1; }; done
 
 # Start server.
-/usr/local/freeswitch/bin/freeswitch -nonat -ncwait -u www-data -g www-data
+/usr/bin/freeswitch -nonat -ncwait -u www-data -g www-data
