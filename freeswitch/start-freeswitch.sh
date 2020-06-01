bc14089563632cd1bc990335144b985e5a44749d
crazyquark <cristian.sandu@gmail.com>
2020-06-01 21:02:23 +0300

    fix start script for mariaDB

--- freeswitch/start-freeswitch.sh
+++ freeswitch/start-freeswitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Wait until PostgreSQL started and listens on port 5432.
-until $(nc -z db 5432); do { printf '.'; sleep 1; }; done
+# Wait until MariaDB started and listens on port 3306.
+until $(nc -z db 3306); do { printf '.'; sleep 1; }; done
 
 # Start server.
 /usr/bin/freeswitch -rp -nonat -u www-data -g www-data
