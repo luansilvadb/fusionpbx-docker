2393e2debc0fc917e95805ab1b4e1d81ed9ece4a
crazyquark <cristian.sandu@gmail.com>
2020-10-25 00:40:05 +0300

    fix dongle permissions

--- freeswitch/start-freeswitch.sh
+++ freeswitch/start-freeswitch.sh
@@ -3,4 +3,4 @@
 until $(nc -z db 5432); do { printf '.'; sleep 1; }; done
 
 # Start server.
-/usr/bin/freeswitch -rp -nonat -u www-data -g www-data
+/usr/bin/freeswitch -rp -nonat -u www-data -g dialout
