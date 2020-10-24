31f0ad0178e8d26154aecbd78e2494745e4c379c
crazyquark <cristian.sandu@gmail.com>
2020-10-24 21:53:09 +0300

    fix start script

--- freeswitch/start-freeswitch.sh
+++ freeswitch/start-freeswitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Wait until MariaDB started and listens on port 3306.
-until $(nc -z db 3306); do { printf '.'; sleep 1; }; done
+# Wait until DB has started.
+until $(nc -z db 5432); do { printf '.'; sleep 1; }; done
 
 # Start server.
 /usr/bin/freeswitch -rp -nonat -u www-data -g www-data
